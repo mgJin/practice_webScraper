9ec81501e687408a45091fa1b799052a57a1068d
mgJin1 <97017695+mgJin@users.noreply.github.com>
2023-04-04 03:39:53 +0000

    search_job_saramin finish more item, also add date

--- main.py
+++ main.py
@@ -1,54 +1,16 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from extractors.saramin import search_saramin
+import datetime
 
-results = []
-job_data = {}
-base_url = "https://www.saramin.co.kr/zf_user/search?searchType=search&searchword="
-search_keyword = "python"
-final_url = f"{base_url}{search_keyword}"
-options = Options()
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+dt = datetime.datetime.now()
+keyword = input("what are you searching for ?")
 
-browser = webdriver.Chrome(options=options)
+results = search_saramin(keyword)
 
-browser.get(final_url)
+file = open(f"searching_{keyword}_job_in_{dt.date()}.csv","w",encoding="utf-8-sig")
+file.write("Title,Link,Location,Career,Education,Worktime,Deadline,Company_link,Company_name\n")
 
-soup = BeautifulSoup(browser.page_source, 'html.parser')
+for result in results:
+  file.write(f"{result['title']},{result['link']},{result['location']},{result['career']},{result['education']},{result['worktime']},{result['deadline']},{result['company_link']},{result['company_name']}\n")
 
-boxs = soup.find_all('div', class_="item_recruit")
+file.close()
 
-for box in boxs:
-  area_job = box.find('div', class_="area_job")
-  area_corp = box.find('div', class_="area_corp")
-  """
-  job_anchors = area_job.select_one("h2 a")
-  title = job_anchors['title']
-  link = job_anchors['href']
-  #print(title)
-  #print(link)
-  job_date =area_job.find('span',class_="date")
-  #print(job_date)
-  #print("\n")
-  job_conditions_big = area_job.find('div',class_="job_condition")
-  
-  if(len(job_conditions_big('span'))==4):
-    loca,carrer,education,worktime = job_conditions_big.find_all('span')
-  elif(len(job_conditions_big('span'))==5):
-    loca,carrer,education,worktime,money = job_conditions_big.find_all('span')
-  else:
-    print("much data")
-  
-  job_locations = loca.find_all('a')
-  location=[]
-  for job_location in job_locations:
-    location.append(job_location.string)
-
-  #print(location)
-  #print("\n")
-  """
-
-  job_company = area_corp.select_one("strong a")
-  company_link = job_company['href']
-  company_name = job_company.string
